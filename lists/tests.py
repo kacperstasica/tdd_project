1e4fe6f000b09aac2b7384202788205a10a60935
Kacper Stasica <kacperstasica@yahoo.pl>
2021-10-19 19:26:10 +0200

    Writing some more tests...

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,14 @@ from django.urls import reverse
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_post_request(self):
+        response = self.client.post(
+            reverse('home'),
+            data={'item_text': 'A new list item'}
+        )
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
