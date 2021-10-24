c986f25e6241bbe018886a7363efad81cfaecf99
Kacper Stasica <kacperstasica@yahoo.pl>
2021-10-24 13:10:02 +0200

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,10 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
-from selenium import webdriver
-import unittest
 
-from selenium.webdriver.common.keys import Keys
-
-
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         # self.fail('Finish the test!') - great thing to remember!
@@ -51,10 +49,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # TODO: Cleanup after FT runs
         # TODO: Support more than one list
-
-
-# checks if it is runned from the command line rather than just imported by another script
-if __name__ == '__main__':
-    # automatically runs unittest test runner, which finds tests, methods and classes in the
-    # file and runs them
-    unittest.main()
