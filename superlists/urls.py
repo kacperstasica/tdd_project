ca8556ec3fc41c46f5ca552b1937d0cb1f0073e7
Kacper Stasica <kacperstasica@yahoo.pl>
2021-11-06 17:06:02 +0100

    Refactor list view to handle new item POSTs

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('lists/', include(lists_urls)),
     path('lists/new', views.new_list, name='new_list'),
     path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
