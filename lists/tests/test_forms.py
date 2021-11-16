85e12acee049d3c04cc16eed6a69ffe134029144
Kacper Stasica <kacperstasica@yahoo.pl>
2021-11-16 11:29:25 +0100

    Made a little refactoring

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
+from lists.models import List, Item
+
 
 class ItemFormTest(TestCase):
     def test_form_item_input_has_placeholder_and_css_classes(self):
@@ -16,3 +18,11 @@ class ItemFormTest(TestCase):
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'to me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'to me')
+        self.assertEqual(new_item.list, list_)
