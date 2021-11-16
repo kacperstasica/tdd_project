67e53969ed1f99836c6f84f4f64769fca003fff1
Kacper Stasica <kacperstasica@yahoo.pl>
2021-11-16 20:46:24 +0100

    Refactor views to use new form (my comment)

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -48,6 +48,7 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys('Buy wellies')
         self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
 
         # user accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies')
