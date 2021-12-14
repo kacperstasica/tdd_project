6cf497fdf296f42954f1bb5585a45ba818fe89fd
Kacper Stasica <kacperstasica@yahoo.pl>
2021-12-14 19:00:41 +0100

    Create page objects for list pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListPage
 
 
 def quit_if_possible(browser):
-    try: browser.quit()
-    except: pass
+    try:
+        browser.quit()
+    except:
+        pass
 
 
 class SharingTest(FunctionalTest):
@@ -26,11 +30,36 @@ class SharingTest(FunctionalTest):
         # kacper goes to home page and creates a list
         self.browser = kacper_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('Get help')
+        list_page = ListPage(self).add_list_item('Get help')
 
         # he notices 'Share this list' option
-        share_box = self.browser.find_element(By.CSS_SELECTOR, 'input[name="sharee"]')
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        # he shares the list with
+        # the page updates to say that it's shared with Onificerous
+        list_page.share_list_with('oni@example.com')
+
+        # Onificerous goes to the list page with his browser
+        self.browser = oni_browser
+        MyListPage(self).go_to_my_lists_page()
+
+        # he sees kacper's list page there
+        self.browser.find_element(By.LINK_TEXT, 'Get help').click()
+
+        # on a list page, Onificerous sees that its kacpers list
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_owner(),
+            'kacper@example.com'
+        ))
+
+        # he adds an item to the list
+        list_page.add_list_item('Hi Kacper!')
+
+        # when kacper refreshes the page, he sees Oni addition
+        self.browser = kacper_browser
+        self.browser.refresh()
+        list_page.wait_for_row_in_list_table('Hi Kacper!', 2)
